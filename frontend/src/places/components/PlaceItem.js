738f5bf0ed92e8200c10342fb24c585877f87b33
Mario Lazzari <mario.lazzari@gmail.com>
2019-12-18 22:06:24 +0100

    Done lesson 51

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 
 import "./PlaceItem.css";
 
@@ -15,10 +16,10 @@ const PlaceItem = props => {
           <h3>{props.address}</h3>
           <p>{props.description}</p>
         </div>
-        <div className="place item__actions">
-          <button>View on map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+        <div className="place-item__actions">
+          <Button inverse>View on map</Button>
+          <Button to={`/places/${props.id}`}>Edit</Button>
+          <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
