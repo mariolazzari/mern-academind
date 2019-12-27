1cff09e99421ca82f2c3a61c608a84ea09df7a61
Mario Lazzari <mario.lazzari@gmail.com>
2019-12-27 23:47:34 +0100

    Done chapter 6

---
+++ node-express/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/user", (req, res, next) => {
+  res.send("<h1>User: " + req.body.username + "</h1>");
+});
+
+app.get("/", (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  );
+});
+
+app.listen(5000);
