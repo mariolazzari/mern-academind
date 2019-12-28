d2a45dd3dc4ea9dd1d601e5eb062bbe233f34e62
Mario Lazzari <mario.lazzari@gmail.com>
2019-12-28 12:37:56 +0100

    Done lesson 92

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,24 @@
 const express = require("express");
+
+// express settings
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/places", require("./routes/places-routes"));
+
+// error handler
+app.use((error, req, res, next) => {
+  if (res.haaderSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "Unknown error." });
+});
+
+// start server
+app.listen(5000, () => {
+  console.log("App listening on port 5000.");
+});
---
+++ backend/routes/places-routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const HttpError = require("../models/http-error");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1"
+  }
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place }); // => { place } => { place: place }
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place });
+});
+
+module.exports = router;
