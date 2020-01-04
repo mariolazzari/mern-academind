6a69a052a2466070c9864a1395b90b5429fa94a8
Mario Lazzari <mario.lazzari@gmail.com>
2020-01-04 18:07:44 +0100

    Done lesson 164

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
 
-const usersController = require('../controllers/users-controllers');
+const usersController = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload.js");
 
 const router = express.Router();
 
-router.get('/', usersController.getUsers);
+router.get("/", usersController.getUsers);
 
 router.post(
-  '/signup',
+  "/signup",
+  fileUpload.single("image"),
   [
-    check('name')
+    check("name")
       .not()
       .isEmpty(),
-    check('email')
+    check("email")
       .normalizeEmail()
       .isEmail(),
-    check('password').isLength({ min: 6 })
+    check("password").isLength({ min: 6 })
   ],
   usersController.signup
 );
 
-router.post('/login', usersController.login);
+router.post("/login", usersController.login);
 
 module.exports = router;
